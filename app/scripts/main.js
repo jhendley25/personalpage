5291d5b524c35fde9ecc6bd68fbe39ced2cf12b8
Jake Hendley <jake.hendley25@gmail.com>
2013-10-18 10:21:07 -0400

    added slidejs plugin and backbone/_.js logos

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,19 @@
-console.log('\'Allo \'Allo!');
+$(function(){
+	$("#slides").slidesjs({
+		width: 400,
+		height: 200,
+		navigation: {
+			active: false
+		},
+		play: {
+          active: false,
+          auto: true,
+          interval: 3000,
+          swap: true
+        },
+        pagination: {
+	      active: false
+		}
+	})
+})
+
